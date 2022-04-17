634a42a092ccd18bbc20a1664fe709810df27209
V6Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2022-04-17 20:22:32 -0300

    L-system text based fixed axion working

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,27 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String axion = "A";
+        Map<Character, String> rules = new HashMap<Character, String>();
+        rules.put('A', "AB");
+        rules.put('B', "A");
+
+        StringBuilder result= new StringBuilder();
+        result.append(axion);
+
+        for(int i = 0; i < 5; i++)
+        {
+            String currentString = result.toString();
+            result.setLength(0);
+            for (char c : currentString.toCharArray())
+            {
+                result.append(rules.get(c));
+            }
+            System.out.println("n = " + i + ": " + result.toString());
+        }
+        
     }
 }
