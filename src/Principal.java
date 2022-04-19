e7cb3ce947f960d6df9ced0b2ca6c91c05abdd9a
V6Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2022-04-19 01:54:13 -0300

    implementado regex para ler arquivo txt

--- src/Principal.java
+++ src/Principal.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
@@ -19,21 +20,34 @@ public class Principal {
             return;
         }
 
-        String axion = "A";
-        Map<Character, String> rules = new HashMap<Character, String>();
-        rules.put('A', "AB");
-        rules.put('B', "A");
+        // obtém os valores da gramática contidas no arquivo txt
+        int iterations = Integer.parseInt(lista.get(0).substring(3).strip());
+        String axion = lista.get(1).substring(3).strip();
 
+        //considere o alfabeto Σ : F f + - [ ]
+        Pattern pattern = Pattern.compile("p\\d : *(F|f|\\+|-|\\[|\\]) *-> *([F|f|\\+|-|\\[|\\]]+)");
+
+        //obtém as regras de produção e as insere em um mapa
+        Map<String, String> rules = new HashMap<String, String>();
+        for(String line : lista){
+            Matcher m = pattern.matcher(line);
+            if(m.find()){
+                rules.put(m.group(1), m.group(2));
+            }
+        }
+
+
+        //constrói em cada iteração a nova string substituindo cada char com base nas regras de produção
         StringBuilder result= new StringBuilder();
         result.append(axion);
-
-        for(int i = 0; i < 5; i++)
+        System.out.println("n = 0: " + axion);
+        for(int i = 1; i <= iterations; i++)
         {
             String currentString = result.toString();
             result.setLength(0);
             for (char c : currentString.toCharArray())
             {
-                result.append(rules.get(c));
+                result.append(rules.get(String.valueOf(c)));
             }
             System.out.println("n = " + i + ": " + result.toString());
         }
@@ -42,14 +56,14 @@ public class Principal {
 
     public static boolean validaLista(List<String> list) {
         // padrão definido da gramática
-        Pattern pattern = Pattern.compile("^(p\\d+|n|ω|δ)\\s:");
+        Pattern pattern = Pattern.compile("^(p\\d+|Σ|n|ω|δ)\\s:");
 
-        if(list.size() < 4)
+        if(list.size() < 5)
             return false;
 
         // checa cada linha do arquivo se bate com o padrão da gramática
         for(String line : list){
-            if(!pattern.matcher(line).matches())
+            if(!pattern.matcher(line).find())
                 return false;
         }
         return true;
